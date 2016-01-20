f9e6af00fc04144630ba2a3ec4b20e9cc5dca602
dauledk <alex@wukwuk.dk>
2016-01-20 16:08:33 +0100

    så kører det

--- From machine to Human.py
+++
@@ -1,7 +0,0 @@
-with open('Scotch data/' + 'Scotch (109x68)') as input_file:
-    next(input_file)  # To skip the first line
-    for whiskey in input_file:
-        whiskey_cleaned = whiskey.replace(" ", "").strip()  # Remove spaces between characters, and '\n'
-        whiskey_tuple_string = tuple(whiskey_cleaned)  # Convert the line to tuple string
-
-        whiskey_tuple_integer = tuple([int(x) for x in whiskey_tuple_string])  # x is a single attribute either 1 or 0

030de075db3d0de350cb1dc04cd2bf5811ec14ff
emilvissing <emil.vissing@icloud.com>
2016-01-29 11:29:11 +0100

    Some additions..
    (cherry picked from commit fa6974c3ea12650ad5caab04c170db0d0abf80c3)

--- dendogram_example.py
+++ dendogram_example.py
@@ -3,6 +3,11 @@ from scipy.cluster.hierarchy import dendrogram, linkage
 from scipy.spatial.distance import pdist
 import numpy as np
 
+
+
+########
+# Guide: https://joernhees.de/blog/2015/08/26/scipy-hierarchical-clustering-and-dendrogram-tutorial/
+######
 np.set_printoptions(precision=5, suppress=True)  # suppress scientific
 
 np.random.seed(4711)  # for repeatability of this tutorial
@@ -13,16 +18,39 @@ X = np.concatenate((a, b),)
 
 Z = linkage(X, 'ward')
 
+def fancy_dendrogram(*args, **kwargs):
+    max_d = kwargs.pop('max_d', None)
 
+    if (max_d) and ('color_threshold' not in kwargs):
+        kwargs['color_threshold'] = max_d
 
-# calculate full dendrogram
-plt.figure(figsize=(25, 10))
-plt.title('Hierarchical Clustering Dendrogram')
-plt.xlabel('sample index')
-plt.ylabel('distance')
-dendrogram(
+    annotate_above = kwargs.pop('annotate_above', 0)
+
+    ddata = dendrogram(*args, **kwargs)
+
+    if not kwargs.get('no_plot', False):
+        plt.title('Hierarchical Clustering Dendrogram (truncated)')
+        plt.xlabel('sample index or (cluster size)')
+        plt.ylabel('distance')
+        for i, d, c in zip(ddata['icoord'], ddata['dcoord'], ddata['color_list']):
+            x = 0.5 * sum(i[1:3])
+            y = d[1]
+            if y > annotate_above:
+                plt.plot(x, y, 'o', c=c)
+                plt.annotate("%.3g" % y, (x, y), xytext=(0, -5),
+                             textcoords='offset points',
+                             va='top', ha='center')
+        if max_d:
+            plt.axhline(y=max_d, c='k')
+    return ddata
+
+fancy_dendrogram(
     Z,
-    leaf_rotation=90.,  # rotates the x axis labels
-    leaf_font_size=8.,  # font size for the x axis labels
+    truncate_mode='lastp',
+    p=12,
+    leaf_rotation=90.,
+    leaf_font_size=12.,
+    show_contracted=True,
+    annotate_above=10,# useful in small plots so annotations don't overlap
 )
 plt.show()

26759833edb54544335ec8727d0fa754a1ac9a48
dauledk <alex@wukwuk.dk>
2016-01-29 16:10:44 +0100

    nu med whiskey

--- Jaccard distance loop.py
+++ Jaccard distance loop.py
@@ -23,17 +23,39 @@ def calculate_euclidean_distance(a, b):
     return distance.jaccard(a_2,b_2)
 
 whiskey_bunnahabhain_tuple = tuple(whiskey_dict['20']['tuple'])  # In the book they wan't to calculate distance from bunnahabhain.
+whiskey_caol_tuple = tuple(whiskey_dict['21']['tuple'])
 
-result_dict = {}
+#  X = np.matrix([[1, 2, 3], [3, 5, 2], [2, 4, 2]])
+
+placeholder_list = []
+for key, whiskey in whiskey_dict.items():
+    placeholder_list.append(whiskey['tuple'])
+
+X = np.matrix(placeholder_list)
+
+Z = linkage(X, 'ward')
+
+plt.figure(figsize=(25, 10))
+plt.title('Hierarchical Clustering Dendrogram')
+plt.xlabel('sample index')
+plt.ylabel('distance')
+dendrogram(
+    Z,
+    leaf_rotation=90.,  # rotates the x axis labels
+    leaf_font_size=8.,  # font size for the x axis labels
+    show_contracted=True,
+)
+
+plt.show()
+
+
+'''
 for number, whiskey in whiskey_dict.items():
     result_dict[whiskey['1. name']] = calculate_euclidean_distance(whiskey_bunnahabhain_tuple, tuple(whiskey['tuple']))
     print(whiskey['1. name'] + " : ", end="")
     print(calculate_euclidean_distance(whiskey_bunnahabhain_tuple, tuple(whiskey['tuple'])))
 
 
-
-plt.scatter()
-plt.show()
 
 
 # for key, whiskey in result_dict.items():
@@ -45,4 +67,4 @@ plt.show()
 #     if whiskey['1. name'] == 'Glenglassaugh':
 #         print(whiskey['1. name'])
 #         print(whiskey['tuple'])
-
+'''
---
+++ play.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+a1 = ()
+a2 = ()
+a
+
+
+np.array(b)
